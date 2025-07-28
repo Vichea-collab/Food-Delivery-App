a0184155bc633740b5575ae376ec34b247d3ceea
Vichea-collab <luchvichea43@gmail.com>
2025-07-28 09:28:12 +0700

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-x3jz.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]); // Start with empty
 
